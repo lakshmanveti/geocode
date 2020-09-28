ac8484acd39d06bad5b5a00f2e85b8900b04a084
Lakshman veti <lakshman.veti@in.arivameddata.com>
2020-09-28 16:27:37 +0530

    added cors

--- web/geo.php
+++ web/geo.php
@@ -1,4 +1,10 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 error_reporting(0);
 $query = $_GET['address'];
 $item_count = $_GET['itemCount'];
