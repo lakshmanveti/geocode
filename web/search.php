47850196a4d24241dc432e0a100796ee06921122
Lakshman veti <lakshman.veti@in.arivameddata.com>
2020-11-11 11:09:33 +0530

    minor fix

--- web/search.php
+++ web/search.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 error_reporting(0);
 $keyword = $_GET['keyword'];
-$url = 'https://www.mapmyindia.com/api/advanced-maps/doc/sample/respatosgst.php?query="'.$keyword.'"';
+$url = 'https://www.mapmyindia.com/api/advanced-maps/doc/sample/respatosgst.php?query=%22'.str_replace(' ', '%2B', $keyword).'%22';
 
 $header = array();
 $header[] = 'Content-length: 0';
@@ -20,7 +20,7 @@ curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($curl, CURLOPT_VERBOSE, 1);
 curl_setopt($curl, CURLOPT_HEADER, 1);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+// curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
 $result = curl_exec($curl);
 
 $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
@@ -30,7 +30,7 @@ $response_header[] = explode("\r\n", substr($result, 0, $header_size));
 $body[] = substr($result, $header_size);
 
 curl_close($curl);
-
+//$res['url'] = $url;
 if ($http_status == '200') {
     $res['status'] = 'success';
     $res['data'] = $body;
@@ -41,5 +41,5 @@ if ($http_status == '200') {
     $res['status'] = 'fail';
     $res['data'] = str_replace("message:", "", $response_header[0][5]);
 }
-echo json_encode($result);
+echo json_encode($res);
 ?>
