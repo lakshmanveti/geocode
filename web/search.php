ac7d7e1d05bf5a7f01692d480b173060b3c45370
Lakshman veti <lakshman.veti@in.arivameddata.com>
2020-11-11 15:31:55 +0530

    minor

--- web/search.php
+++ web/search.php
@@ -2,7 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Allow-Headers: X-PINGOTHER, Content-Type');
     header('Access-Control-Max-Age: 1728000');
     header('Content-Length: 0');
     header('Content-Type: text/plain');
@@ -11,9 +11,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 header('Access-Control-Allow-Origin: *');
 
-header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 
-header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Headers: X-PINGOTHER, Content-Type");
 
 error_reporting(0);
 $keyword = $_GET['keyword'];
